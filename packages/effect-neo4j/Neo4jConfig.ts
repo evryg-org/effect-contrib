968a135efc56bac9b2e9d2e658b17c4d9302e8f0
Jean-Baptiste Musso <jeanbaptiste@evryg.com>
2026-04-11 22:03:51 +0200

    Add Neo4jConnectionConfig to effect-neo4j exports

--- packages/effect-neo4j/Neo4jConfig.ts
+++ packages/effect-neo4j/Neo4jConfig.ts
@@ -6,3 +6,5 @@ export class Neo4jConfig extends Context.Tag("Neo4jConfig")<Neo4jConfig, {
   readonly password: string
   readonly database: string
 }>() {}
+
+export type Neo4jConnectionConfig = Context.Tag.Service<typeof Neo4jConfig>
